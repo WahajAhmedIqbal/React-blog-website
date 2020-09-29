54758001e7683fa764fd1f8f2082b8bb7766ccc6
WahajAhmedIqbal <wahaj465@gmail.com>
2020-09-30 03:12:33 +0500

    pages

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function index() {
+  return (
+    <div className="home">
+      <p>this is home</p>
+    </div>
+  );
+}
+
+export default index;
